1746259a061667d11fbf1a560af316a3057a02be
Przemek Urbaniak <przemyslaw.urbaniak@solwit.com>
2016-10-04 20:51:57 +0200

    Reading temperatures form one sensor

--- serial-temperature.ino
+++ serial-temperature.ino
@@ -1,12 +1,31 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 3
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // Start up the library
+  sensors.begin();
   while(!Serial);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  Serial.println("lkkk");
+
+
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println(sensors.getTempCByIndex(0)); 
   delay(500);
 }
